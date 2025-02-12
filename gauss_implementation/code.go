eeed95aae13e25edcd43e63a5c25c6caae31ce78
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-02-12 19:52:12 -0300

    Create code.go

---
+++ gauss_implementation/code.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const MAXN = 2000 
+var N int 
+var A [MAXN][MAXN]float32 
+var B [MAXN]float32
+var X [MAXN]float32
+
+
+func gauss() {
+}
+
+
+func main() {
+
+gauss()
+
+
+}

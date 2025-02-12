ea7246036bd6424696ce6169a6938c00d5aa0070
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-02-12 19:47:51 -0300

    Create code.rs

---
+++ Opção B/code.rs
@@ -0,0 +1,18 @@
+use std::time::{Instant};
+
+const MAXN: usize = 2000; 
+static mut N: usize = 0;
+static mut A: [[f32; MAXN]; MAXN] = [[0.0; MAXN]; MAXN];
+static mut B: [f32; MAXN] = [0.0; MAXN];
+static mut X: [f32; MAXN] = [0.0; MAXN];
+
+
+fn gauss(){
+}
+
+
+fn main(){
+
+gauss();
+
+}
